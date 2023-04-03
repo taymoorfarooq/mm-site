efbe0b118b753c3135b44d9b8b4e0dea08fd21e2
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2023-04-03 06:17:53 -0400

    loading animation

--- src/App.js
+++ src/App.js
@@ -21,4 +21,12 @@ function App() {
   )
 }
 
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    }, 2000);
+}) 
+
 export default App;
